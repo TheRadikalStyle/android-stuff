ac9b51d073ff995f0dac8c543351efc0ed2a8eb7
David Ochoa <radikal_david@hotmail.com>
2020-08-14 11:34:27 -0500

    Updated Tools

--- snippets/Tools.java
+++ snippets/Tools.java
@@ -20,6 +20,8 @@ import java.util.List;
 
 public class Tools {
     private static Tools INSTANCE = null;
+    private int counter = 0;
+    private boolean isEEggAlreadyShow = false;
 
     private Tools(){}
 
@@ -169,4 +171,37 @@ public class Tools {
         }).show();
     }else{} */
   }
+
+ public void SetEasterEgg(Context ctx, TextView versionTXV){
+        //Get App Version
+        try {
+            PackageInfo pInfo = ctx.getPackageManager().getPackageInfo(ctx.getPackageName(), 0);
+            String version = pInfo.versionName;
+            versionTXV.setText("v."+version);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        versionTXV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter++;
+                if(counter >= 7){
+                    if(!isEEggAlreadyShow){
+                        Calendar cal = Calendar.getInstance();
+                        int hour = cal.get(Calendar.HOUR);
+                        int minute = cal.get(Calendar.MINUTE);
+                        if(hour == 3 && minute == 0){
+                            Toast.makeText(ctx, "Dev by: David Ochoa", Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(ctx, "" + ("\ud83d\ude03"), Toast.LENGTH_SHORT).show(); //Show :D emoji
+                        }
+                        isEEggAlreadyShow = true;
+                    }
+                    counter = 0;
+                }
+            }
+        });
+    }
+
 }
